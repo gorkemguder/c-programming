c48a423ec970b3f67c0c404d2fd561db23ca694d
gorkemguder <117344704+gorkemguder@users.noreply.github.com>
2022-11-15 00:21:23 +0300

    Update program.c

--- sesli-harf-bul/program.c
+++ sesli-harf-bul/program.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main () {
+	char karakter;
+	printf("Karakter giriniz: ");
+	scanf("%c", &karakter);
+	if (karakter == 'a' || karakter == 'e' || karakter == 'o' || karakter == 'u' || karakter == 'i'){
+		printf("%c karakteri sesli harftir.", karakter);
+	}
+	else if (karakter != 'a' || karakter != 'e' || karakter != 'o' || karakter != 'u' || karakter != 'i') {
+		printf("%c karakteri sesli harf degildir.", karakter);
+	}
+}
