4bdb765add99c59717363ac4e0992b3c3d9863fc
gorkemguder <117344704+gorkemguder@users.noreply.github.com>
2022-11-15 00:29:43 +0300

    Update program.c

--- sayi-cift-tek-bul/program.c
+++ sayi-cift-tek-bul/program.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+	int sayi;
+	printf("Sayi giriniz:");
+	scanf("%d", &sayi);
+	if (sayi % 2 == 0){
+		printf("%d sayisi cift sayidir.", sayi);
+	}
+	else if (sayi % 2 != 0){
+		printf("%d sayisi tek sayidir.", sayi);
+	}
+	else {
+		printf("Hata!");
+	}
+}
